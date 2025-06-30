62b427ba1bf069d1a0b1145a69f152b760e1b172
MartaRobledoLab <marta.robledo@unican.es>
2025-06-30 17:59:16 +0200

    Update pipeline.sh

--- Script/pipeline.sh
+++ Script/pipeline.sh
@@ -2,21 +2,21 @@
 
 if true ; then
 
-./q2_v4_run4.sh 2>&1 | tee q2_v4.log
-./q2_v4_run5.sh 2>&1 | tee -a q2_v4.log
-./q2_v4_run6.sh 2>&1 | tee -a q2_v4.log
+Script/q2_v4_run4.sh 2>&1 | tee q2_v4.log
+Script/q2_v4_run5.sh 2>&1 | tee -a q2_v4.log
+Script/q2_v4_run6.sh 2>&1 | tee -a q2_v4.log
 
 fi
 
 if true ; then
 
-./q2_v4_merg.sh 2>&1 | tee -a q2_v4.log
+Script/q2_v4_merg.sh 2>&1 | tee -a q2_v4.log
 
 fi
 
 if true ; then
 
-./Metabarcoding_analysis.R
-./qPCR.R
+Script/Metabarcoding_analysis.R
+Script/qPCR.R
 
 fi
